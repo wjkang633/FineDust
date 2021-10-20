e6093011186b6f1bb25a6b2ed075715e54d5dbda
kwjjjjj <kwjjjjj@abrain.co.kr>
2021-10-20 15:40:40 +0900

    [added] 받아온 데이터 가지고 뷰 그리기

--- app/src/main/java/woojin/android/kotlin/project/finedust/MainActivity.kt
+++ app/src/main/java/woojin/android/kotlin/project/finedust/MainActivity.kt
@@ -16,6 +16,9 @@ import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.cancel
 import kotlinx.coroutines.launch
 import woojin.android.kotlin.project.finedust.data.Repository
+import woojin.android.kotlin.project.finedust.data.models.airquality.AirQuality
+import woojin.android.kotlin.project.finedust.data.models.airquality.Grade
+import woojin.android.kotlin.project.finedust.data.models.monitoringstation.MonitoringStation
 import woojin.android.kotlin.project.finedust.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -88,20 +91,59 @@ class MainActivity : AppCompatActivity() {
                 cancellationTokenSource!!.token
             )
             .addOnSuccessListener { location ->
-                binding.textView.text = "${location.latitude}, ${location.longitude}"
-
                 //코루틴 시작
                 scope.launch {
                     val monitoringStation =
                         Repository.getNearbyMonitoringStation(location.latitude, location.longitude)
 
-                    Log.e("우진", "${monitoringStation?.stationName}")
+                    val airQuality =
+                        Repository.getLatestAirQualityData(monitoringStation!!.stationName!!)
 
-                    val airQuality = Repository.getLatestAirQualityData(monitoringStation!!.stationName!!)
-
-                    binding.textView.text = airQuality.toString()
+                    displayAirQualityData(monitoringStation, airQuality!!)
                 }
             }
+    }
+
+    fun displayAirQualityData(monitoringStation: MonitoringStation, airQuality: AirQuality) {
+        binding.measuringStationNameTextView.text = monitoringStation.stationName
+        binding.measuringStationAddressTextView.text = monitoringStation.addr
+
+        //종합 지수
+        (airQuality.khaiGrade ?: Grade.UNKNOWN)?.let { grade ->
+            binding.root.setBackgroundResource(grade.colorResId)
+            binding.totalGradeLabelTextView.text = grade.label
+            binding.totalGradeEmojiTextView.text = grade.emoji
+        }
+
+
+        //미세, 초미세 등
+        with(airQuality) {
+            binding.fineDustInfoTextView.text =
+                "미세먼지: $pm10Value ㎍/㎥ ${(pm10Grade ?: Grade.UNKNOWN).emoji}."
+            binding.ultraFineDustInfoTextView.text =
+                "초미세먼지: $pm25Value ㎍/㎥ ${(pm25Grade ?: Grade.UNKNOWN).emoji}."
+
+            with(binding.so2Item) {
+                labelTextView.text = "아황산가스"
+                gradeTextView.text = (so2Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$so2Value ppm"
+            }
+            with(binding.coItem) {
+                labelTextView.text = "일산화탄소"
+                gradeTextView.text = (coGrade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$coValue ppm"
+            }
+            with(binding.o3Item) {
+                labelTextView.text = "오존"
+                gradeTextView.text = (o3Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$o3Value ppm"
+            }
+            with(binding.no2Item) {
+                labelTextView.text = "이산화질소"
+                gradeTextView.text = (no2Grade ?: Grade.UNKNOWN).toString()
+                valueTextView.text = "$no2Value ppm"
+            }
+        }
     }
 
     companion object {
